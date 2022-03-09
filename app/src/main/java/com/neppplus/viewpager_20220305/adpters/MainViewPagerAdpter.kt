5e915d23e1405fb24fbaa6cb8d8faa94406a8303
BradlyKim <kjh5864@naver.com>
2022-03-09 18:54:52 +0900

    뷰페이저 어댑터 클래스 추가 중  - 상속 받은 부모의 생성자에 필요한 재료 맞춰주기

---
+++ app/src/main/java/com/neppplus/viewpager_20220305/adpters/MainViewPagerAdpter.kt
@@ -0,0 +1,10 @@
+package com.neppplus.viewpager_20220305.adpters
+
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+
+class MainViewPagerAdpter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+
+
+
+}
