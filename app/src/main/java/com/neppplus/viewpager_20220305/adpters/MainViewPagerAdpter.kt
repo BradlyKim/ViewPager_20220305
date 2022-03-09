5fe35441a4bc9c70c7dbd2a234ce643b45c4d629
BradlyKim <kjh5864@naver.com>
2022-03-09 19:05:34 +0900

    상속에 의한 필수 구현함수 2개 오버라이딩

--- app/src/main/java/com/neppplus/viewpager_20220305/adpters/MainViewPagerAdpter.kt
+++ app/src/main/java/com/neppplus/viewpager_20220305/adpters/MainViewPagerAdpter.kt
@@ -1,10 +1,17 @@
 package com.neppplus.viewpager_20220305.adpters
 
+import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentPagerAdapter
 
 class MainViewPagerAdpter(fm : FragmentManager) : FragmentPagerAdapter(fm) {
+    override fun getCount(): Int {
 
+    }
+
+    override fun getItem(position: Int): Fragment {
+
+    }
 
 
 }
